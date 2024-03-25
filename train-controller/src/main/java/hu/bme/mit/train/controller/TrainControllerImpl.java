8d874a93024730da3b89029ea689082c83fdbb67
Csongorcz <csongorczibuly33@gmail.com>
2024-03-25 16:45:13 +0100

    Javasolt módosítás elvégezve

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -19,7 +19,7 @@ public class TrainControllerImpl implements TrainController {
 				try
 				{
 					followSpeed();
-					thread.sleep(2000);
+					thread.sleep(1000);
 				}
 				catch(InterruptedException e)
 				{
